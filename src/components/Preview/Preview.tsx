2a9cde515df2599d6af06226dfb6771e5ba28d99
Joseph Maynard <joseph.maynard@gmail.com>
2024-04-18 23:34:33 +0100

    Fix container query sizes

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -1,3 +1,5 @@
+import './Preview.css';
+
 export interface Props {
   children: React.ReactNode;
   keyColour: string;
@@ -7,7 +9,7 @@ export default function Preview({ children, keyColour }: Props) {
   return (
     <div className="bg-black">
       <div
-        className="relative mx-auto aspect-video max-w-3xl"
+        className="Preview relative mx-auto aspect-video max-w-3xl"
         style={{ backgroundColor: keyColour }}
       >
         {children}
