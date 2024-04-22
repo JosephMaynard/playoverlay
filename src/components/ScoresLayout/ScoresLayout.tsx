d4d3b36755372ec00b464bfd6d6f8d07690a8e73
Joseph Maynard <joseph.maynard@gmail.com>
2024-04-22 21:10:08 +0100

    Add reveal animation to score layout

--- src/components/ScoresLayout/ScoresLayout.tsx
+++ src/components/ScoresLayout/ScoresLayout.tsx
@@ -6,15 +6,16 @@ export interface Props {
   scores: Scores;
   settings: Settings;
   time: Time;
+  active: boolean;
 }
 
-export default function Scores({ scores, settings, time }: Props) {
+export default function Scores({ scores, settings, time, active }: Props) {
   return (
-    <div className="ScoresLayout flex">
+    <div className={`ScoresLayout flex${active ? ' ScoresLayout_active' : ''}`}>
       <ScoresTeamName
         textColour={settings.homeTeamTextColour}
         backgroundColour={settings.homeTeamBackgroundColour}
-        teamName={settings.homeTeamName}
+        teamName={settings.homeTeamNameAbbreviated}
       />
       <div className="ScoresLayout_item ScoresLayout_scores bg-black text-center font-bold text-white">
         {' '}
@@ -23,11 +24,16 @@ export default function Scores({ scores, settings, time }: Props) {
       <ScoresTeamName
         textColour={settings.awayTeamTextColour}
         backgroundColour={settings.awayTeamBackgroundColour}
-        teamName={settings.awayTeamName}
+        teamName={settings.awayTeamNameAbbreviated}
       />
       {time.time && (
         <div className="ScoresLayout_item ScoresLayout_time bg-black text-center text-white">
           {time.time}
+        </div>
+      )}
+      {time.additionalTime && (
+        <div className="ScoresLayout_item bg-white text-center font-bold text-black">
+          + {time.additionalTime}
         </div>
       )}
     </div>
